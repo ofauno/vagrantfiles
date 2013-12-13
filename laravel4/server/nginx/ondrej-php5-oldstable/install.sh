11bc1a3ab2d039f91a364105f854b3e725090210
faunus <faunotv@gmail.com>
2013-12-12 21:50:48 -0600

    stop apache after install all php dependencies

--- laravel4/server/nginx/ondrej-php5-oldstable/install.sh
+++ laravel4/server/nginx/ondrej-php5-oldstable/install.sh
@@ -3,6 +3,20 @@
 echo "--- Updating packages list ---"
 sudo apt-get update
 
+echo "--- Setting nginx ---"
+sudo add-apt-repository -y ppa:nginx/stable
+sudo apt-get update
+sudo apt-get install -y nginx
+
+echo "--- Setting document root ---"
+sudo rm -rf /var/www
+sudo ln -fs /vagrant/www/public /var/www
+
+sudo rm /etc/nginx/sites-available/default
+sudo ln -fs /vagrant/server/nginx/default /etc/nginx/sites-available/default
+
+echo "--- Restarting nginx ---"
+
 echo "--- MySQL ---"
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
@@ -22,6 +36,12 @@ sudo apt-get update
 echo "--- Installing PHP-specific packages ---"
 sudo apt-get install -y php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core
 
+# echo "--- Stop Apache ---"
+sudo service apache2 stop
+
+sudo apt-get install -y php5-fpm
+sudo service nginx start
+
 echo "--- Installing and configuring Xdebug ---"
 sudo apt-get install -y php5-xdebug
 
@@ -30,24 +50,6 @@ xdebug.scream=1
 xdebug.cli_color=1
 xdebug.show_local_vars=1
 EOF
-
-echo "--- Setting nginx ---"
-sudo add-apt-repository -y ppa:nginx/stable
-sudo apt-get update
-sudo apt-get install -y nginx
-
-echo "--- Setting document root ---"
-sudo rm -rf /var/www
-sudo ln -fs /vagrant/www/public /var/www
-
-sudo rm /etc/nginx/sites-available/default
-sudo ln -fs /vagrant/server/nginx/default /etc/nginx/sites-available/default
-
-echo "--- Restarting nginx ---"
-
-sudo apt-get install -y php5-fpm
-
-sudo service nginx start
 
 echo "--- installing Composer ---"
 curl -sS https://getcomposer.org/installer | php
